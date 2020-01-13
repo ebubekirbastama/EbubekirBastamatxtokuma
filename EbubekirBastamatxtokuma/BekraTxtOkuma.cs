966d93d24b9eb4f3072133c8d5148a4642a8a266
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-01-13 11:15:01 +0300

    Add files via upload

---
+++ EbubekirBastamatxtokuma/BekraTxtOkuma.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EbubekirBastamatxtokuma
+{
+    public class BekraTxtOkuma
+    {
+        //*İletişim ve Eğitim Adreslerimiz..*//
+        //*---------------------------------*//
+        //Contact : +90 5554128854;
+        //Whatshapp Contact : +90 5554128854;
+        //Email Contact : ebubekirbastama@elmalicesmekuruyemis.com
+        //Website Contact : https://www.ebubekirbastama.com
+        //Education https://www.youtube.com/user/yazilimegitim
+        //Education https://www.youtube.com/channel/UC240A7DHqgAR8bEMakGdqWg/videos?view_as=subscriber
+
+        #region Değişkenler
+            public Encoding en { get; set; }
+
+        #endregion
+
+        private void txt_aktar(string DosyaYolu, ListBox lst, bool encode)
+        {
+            if (encode == false)
+            {
+                string[] vrr = File.ReadAllLines(DosyaYolu, Encoding.GetEncoding("iso-8859-9"));
+                foreach (var item in vrr)
+                {
+                    lst.Items.Add(item);
+                }
+            }
+            else
+            {
+                string[] vrr = File.ReadAllLines(DosyaYolu, en);
+                foreach (var item in vrr)
+                {
+                    lst.Items.Add(item);
+                }
+            }
+
+        }
+        private string[] txt_aktar_dizi(string DosyaYolu, bool encode)
+        {
+            if (encode == false)
+            {
+                string[] dizi = File.ReadAllLines(DosyaYolu, Encoding.GetEncoding("iso-8859-9"));
+                return dizi;
+            }
+            else
+            {
+                string[] dizi = File.ReadAllLines(DosyaYolu, en);
+                return dizi;
+            }
+
+        }
+
+        public void Txt_Aktar(string DosyaYolu, ListBox lst, bool encode)
+        {
+            txt_aktar(DosyaYolu, lst, encode);
+        }
+        public string[] Txt_Aktar_Dizi(string DosyaYolu, bool encode)
+        {
+            return txt_aktar_dizi(DosyaYolu, encode);
+        }
+    }
+}
